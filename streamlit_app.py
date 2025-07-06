7c1b3d90e300be1d06dc4180d711d83a289689aa
vishnu18ro <vishnu18ro@gmail.com>
2025-07-06 19:47:39 +0530

    Updated requirements and streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Welcome to your BRO 😎")
+st.subheader("A friendly companion")
+st.text_input("Ask something:")
+st.markdown("📌 For help and inspiration, visit [docs.streamlit.io](https://docs.streamlit.io/)")
